dad3320edd5c6dba37fcdf8e864bfa5105dd3fd2
Rafael Miranda <rafaelpadovezi@gmail.com>
2022-03-18 17:55:26 -0300

    tests: add publisher test example

--- src/Ecommerce.Poc.Catalog/Commands/ApiCommand.cs
+++ src/Ecommerce.Poc.Catalog/Commands/ApiCommand.cs
@@ -34,7 +34,6 @@ namespace Ecommerce.Poc.Catalog.Commands
             // This method gets called by the runtime. Use this method to add services to the container.
             public void ConfigureServices(IServiceCollection services)
             {
-
                 services.AddControllers();
                 services.AddSwaggerGen(c =>
                 {
